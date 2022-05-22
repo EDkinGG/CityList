2f322f497dac301e1a4c02a3823735afb6fd01eb
EDkinGG <r23ghani@gmail.com>
2022-05-22 14:36:45 +0600

    unit test by 91

--- app/src/main/java/com/example/citylist/CityList.java
+++ app/src/main/java/com/example/citylist/CityList.java
@@ -2,10 +2,15 @@ package com.example.citylist;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * This is a class that keeps track of a list of city objects
+ * @auther Ghani91
+ * @version 3.1.2
+ * @since 2022
+ * @see <a href = "https://docs.oracle.com/en/java/javase/14/docs/api/index.html">Java Docs</a>
  */
 public class CityList {
     private List<City> cities = new ArrayList<>();
@@ -23,13 +28,58 @@ public class CityList {
     }
 
     /**
+     * This deletes a city from the list if that city exists
+     * @param city
+     *      This is the city to delete
+     */
+    public void delete(City city )//---RS
+    {
+        if( !cities.contains(city) )
+        {
+            throw new IllegalArgumentException();
+        }
+        else
+        {
+            cities.remove(city);
+        }
+    }
+
+
+    /**
      * This returns a sorted list of cities
+     * @param param
+     *         if the param is 1 the list is sorted by city
+     *         else the list is sorted by province
      * @return
      *      Return the sorted list of cities
      */
-    public List<City> getCities() {
+    public List<City> getCities( int param ) //---RS
+    {
         List<City> cityList = cities;
-        Collections.sort(cityList);
+        if( param == 1 )
+        {
+            Collections.sort(cityList);
+        }
+        else
+        {
+            Collections.sort(cityList, new Comparator<City>() {
+                @Override
+                public int compare(City city, City t1) {
+                    return city.getProvinceName().compareTo(t1.getProvinceName());
+                }
+            });
+        }
         return cityList;
     }
+
+    /**
+     * This returns a count of the Cities from the list
+     * @return
+     *      Return the count of the Cities from the list
+     */
+    public int count()//---RS
+    {
+        return cities.size();
+    }
+
 }
--- app/src/test/java/com/example/citylist/CityListTest.java
+++ app/src/test/java/com/example/citylist/CityListTest.java
@@ -18,13 +18,13 @@ public class CityListTest {
     @Test
     public void testAdd() {
         CityList cityList = mockCityList();
-        assertEquals(1, cityList.getCities().size());
+        assertEquals(1, cityList.getCities(1).size());
 
         City city = new City("Regina", "SK");
         cityList.add(city);
 
-        assertEquals(2, cityList.getCities().size());
-        assertTrue(cityList.getCities().contains(city));
+        assertEquals(2, cityList.getCities(1).size());
+        assertTrue(cityList.getCities(1).contains(city));
     }
 
     @Test
@@ -39,14 +39,85 @@ public class CityListTest {
     }
 
     @Test
-    public void testGetCities() {
+    public void testdelete()//---RS
+    {
+        CityList cList = new CityList();
+        City c1 = new City("Mumbai","Maharashtra");
+        City c2 = new City("Kolkata","West Bengal");
+
+        cList.add(c1);
+        cList.add(c2);
+
+        cList.delete(c1);
+        assertTrue(!cList.getCities(1).contains(c1));
+    }
+
+    @Test
+    public void testDeleteException()//----RS
+    {
+        CityList cList = new CityList();
+        City c1 = new City("Mumbai","Maharashtra");
+        City c2 = new City("Kolkata","West Bengal");
+
+        cList.add(c1);
+        cList.add(c2);
+
+        cList.delete(c1);
+        assertThrows(IllegalArgumentException.class, () -> {
+            cList.delete(c1);
+        });
+    }
+    @Test
+    public void testcount()//-----RS
+    {
+        CityList cList = new CityList();
+        City c1 = new City("Mumbai","Maharashtra");
+        City c2 = new City("Kolkata","West Bengal");
+        City c3 = new City("Chennai","Tamil Nadu");
+
+        cList.add(c1);
+        cList.add(c2);
+        cList.add(c3);
+        assertEquals(3,cList.count());
+
+        cList.delete(c1);
+        assertEquals(2,cList.count());
+
+        cList.delete(c2);
+        cList.delete(c3);
+        assertEquals(0,cList.count());
+    }
+
+    @Test
+    public void testGetCities()//-----RS
+    {
         CityList cityList = mockCityList();
-        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities(1).get(0)));
 
         City city = new City("Charlottetown", "Prince Edward Island");
         cityList.add(city);
 
-        assertEquals(0, city.compareTo(cityList.getCities().get(0)));
-        assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));
+        assertEquals(0, city.compareTo(cityList.getCities(1).get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities(1).get(1)));
+    }
+
+    @Test
+    public void testSortedCities()
+    {
+        CityList cityList = new CityList();
+        City c1= new City("AA", "ZZ");
+        cityList.add(c1);
+        assertEquals(0, c1.compareTo(cityList.getCities(1).get(0)));//c1 comparing with the index zero of the sorted array(get(0) diye index 0 er value antesi)
+
+        City c2 = new City("DD", "YY");
+        cityList.add(c2);
+        //city wise sorting test
+        assertEquals(0, c1.compareTo(cityList.getCities(1).get(0)));
+        assertEquals(0, c2.compareTo(cityList.getCities(1).get(1)));
+        //province wise sorting test
+        assertEquals(0, c1.compareTo(cityList.getCities(2).get(1)));
+        assertEquals(0, c2.compareTo(cityList.getCities(2).get(0)));
+
+
     }
 }
